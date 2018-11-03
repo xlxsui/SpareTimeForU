0f450c06f4ee0bb19d1b40fa56a56ad6ec620912
xlxs123 <17wjli6@stu.edu.cn>
2018-11-03 23:12:30 +0800

    change the STFUFragment

--- app/src/main/java/com/sparetimeforu/android/sparetimeforu/ErrandFragment.java
+++ app/src/main/java/com/sparetimeforu/android/sparetimeforu/ErrandFragment.java
@@ -3,6 +3,7 @@ package com.sparetimeforu.android.sparetimeforu;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,10 +28,19 @@ public class ErrandFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_errand_main, container, false);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_errand_main_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+//        setupAdapter();
 
         return view;
     }
 
+    private class ErrandHolder extends RecyclerView.ViewHolder {
+
+        public ErrandHolder(View itemView) {
+            super(itemView);
+        }
+    }
 
 
 }
