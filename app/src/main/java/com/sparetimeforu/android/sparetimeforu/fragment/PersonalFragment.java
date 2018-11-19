734f4cdb3a7447f44399da37e70727606f8b23b9
HuQingYuan <17qyhu@stu.edu.cn>
2018-11-19 21:17:32 +0800

    pull down and reflash

--- app/src/main/java/com/sparetimeforu/android/sparetimeforu/fragment/PersonalFragment.java
+++ app/src/main/java/com/sparetimeforu/android/sparetimeforu/fragment/PersonalFragment.java
@@ -3,6 +3,7 @@ package com.sparetimeforu.android.sparetimeforu.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.sparetimeforu.android.sparetimeforu.R;
 import com.sparetimeforu.android.sparetimeforu.ServerConnection.OkHttpUtil;
@@ -32,13 +34,15 @@ public class PersonalFragment extends Fragment {
     public static String LoginServerUrl=" https://www.easy-mock.com/mock/5bebc836e0c6d321cade7458/success/LoginServer";
 
     //用户个人信息
-    TextView personal_favourable_rate,personal_nickname,personal_signate;
-    ImageView personal_avator;
+    private TextView personal_favourable_rate,personal_nickname,personal_signate;
+    private ImageView personal_avator;
 
     //用户界面需要相应的控件
-    TextView personal_mission_received,personal_mission_released;
+    private TextView personal_mission_received,personal_mission_released;
 
-    User user;
+    private User user;
+    private Toolbar toolbar;
+    private ImageView personal_go_back;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -53,6 +57,22 @@ public class PersonalFragment extends Fragment {
         personal_favourable_rate=(TextView) view.findViewById(R.id.personal_favourable_rate);
         initDate();
 
+
+        //设置Toolbar
+        toolbar=view.findViewById(R.id.personal_toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+
+
+        //设置Toolbar中返回按钮的点击事件
+
+        personal_go_back=(ImageView)view.findViewById(R.id.persnal_go_back);
+        personal_go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
         return view;
     }
 
